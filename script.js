d7ba210a73708bafc9a683895741e543e73ecdc4
robertsoumx <robertsou2023@outlook.com>
2026-01-10 14:08:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-function renderArticles() {
-  const list = document.getElementById("article-list");
-  const q = document.getElementById("search").value.toLowerCase();
-  const sort = document.getElementById("sort").value;
-
-  let data = Object.entries(articles)
-    .filter(([_, a]) =>
-      a.title.toLowerCase().includes(q) ||
-      a.author.toLowerCase().includes(q)
-    );
-
-  data.sort((a, b) => a[1][sort].localeCompare(b[1][sort]));
-
-  list.innerHTML = "";
-  data.forEach(([id, a]) => {
-    list.innerHTML += `
-      <div class="article-list-item" onclick="openArticle('${id}')">
-        <h3>${a.title}</h3>
-        <div class="article-meta">${a.author} · ${a.date}</div>
-      </div>`;
-  });
-}
-// Redirect to article page
 function openArticle(id) {
   window.location.href = `article.html?id=${id}`;
 }
